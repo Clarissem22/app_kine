062c05423ab4fad386950e7a70d56c7e857ef7e8
step31 <cmille749@gmail.com>
2022-03-28 09:42:06 +0200

    cache:clear

--- migrations/Version20220328074123.php
+++ migrations/Version20220328074123.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220327115532 extends AbstractMigration
+final class Version20220328074123 extends AbstractMigration
 {
     public function getDescription(): string
     {
